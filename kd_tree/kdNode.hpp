3e6632811e7cf772a9129917b1da825d39cbc747
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-09-16 21:58:53 -0500

    -Code formated with LLVM style

--- kd_tree/kdNode.hpp
+++ kd_tree/kdNode.hpp
@@ -7,24 +7,24 @@ using namespace std;
 using Point = vector<string>;
 
 class KDNode {
-   public:
-    Point data;
-    KDNode* childs[2];
+public:
+  Point data;
+  KDNode *childs[2];
 
-    KDNode() {
-        data = Point(0);
-        childs[0] = childs[1] = nullptr;
-    }
+  KDNode() {
+    data = Point(0);
+    childs[0] = childs[1] = nullptr;
+  }
 
-    KDNode(const Point& data) {
-        this->data = data;
-        childs[0] = childs[1] = nullptr;
-    }
+  KDNode(const Point &data) {
+    this->data = data;
+    childs[0] = childs[1] = nullptr;
+  }
 
-    KDNode(Point&& data) {
-        this->data = move(data);
-        childs[0] = childs[1] = nullptr;
-    }
+  KDNode(Point &&data) {
+    this->data = move(data);
+    childs[0] = childs[1] = nullptr;
+  }
 
-    ~KDNode(){};
+  ~KDNode(){};
 };
--- kd_tree/kdTree.hpp
+++ kd_tree/kdTree.hpp
@@ -3,24 +3,23 @@
 #include "utils.hpp"
 
 class KDTree {
-   public:
-    KDNode* root;
-    size_t dimensions;
-    size_t depth;
-    vector<Types> types;
+public:
+  KDNode *root;
+  size_t dimensions;
+  size_t depth;
+  vector<Types> types;
 
-    KDTree() { root = nullptr; }
+  KDTree() { root = nullptr; }
 
-    KDTree(vector<Point>& points, const size_t depth);
-    KDNode* buildTree(vector<Point>& points, const size_t dimensions,
-                      const size_t maxDepth);
-    KDNode* buildSubTrees(vector<Point>& points,
-                          vector<vector<size_t> >& sortedIdxs,
-                          const size_t first, const size_t last,
-                          const size_t dimensions, const size_t maxDepth,
-                          const size_t crrntDepth);
+  KDTree(vector<Point> &points, const size_t depth);
+  KDNode *buildTree(vector<Point> &points, const size_t dimensions,
+                    const size_t maxDepth);
+  KDNode *buildSubTrees(vector<Point> &points,
+                        vector<vector<size_t>> &sortedIdxs, const size_t first,
+                        const size_t last, const size_t dimensions,
+                        const size_t maxDepth, const size_t crrntDepth);
 
-    KDNode* find(const Point& srchPoint);
-    void print();
-    ~KDTree();
+  KDNode *find(const Point &srchPoint);
+  void print();
+  ~KDTree();
 };
